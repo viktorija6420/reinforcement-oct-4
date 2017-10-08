827d3ed3bfcb381723c93039b23fe9d8a3eb1ace
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-10-08 06:19:11 -0400

    fixed the bug

---
+++ exercise.js
@@ -0,0 +1,12 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+console.log("Todo:");
+for(var i = 0; i < items.length; i++){
+   console.log((i+1) + ': ' + items[i]);
+}
+
+// The output should be:
+// Todo:
+// 1: purchase domain
+// 2: install SSL certificate
+// 3: deploy site
+// 4: celebrate success
